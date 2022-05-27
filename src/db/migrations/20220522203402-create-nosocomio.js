30b44dc98acba56272aa2c04088e68519366dcc4
GDiamend <Gdiamend@gmail.com>
2022-05-26 22:01:12 -0300

    agregue la definicion de la tabla

---
+++ src/db/migrations/20220522203402-create-nosocomio.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Nosocomios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Users', key: 'id' },
+        allowNull: false
+      },
+      bussinesName: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Nosocomios');
+  }
+};
---
+++ src/db/seeders/20220522204638-Nosocomio.js
@@ -0,0 +1,58 @@
+'use strict';
+const { randFirstName,randLastName,randPhoneNumber,randPassword,randEmail,randNumber, randStreetName, randCity, randCountry, randCompanyName} = require('@ngneat/falso');
+
+let direccion = {};
+let usuario = {};
+let nosocomio = {};
+
+module.exports = {
+  async up (queryInterface, Sequelize) {  
+    for(var i = 0;i<50;i++){
+      direccion = {
+        street: randStreetName(),
+        number: randNumber({ min: 1,max:9999 }),
+        floor: randNumber({ min: 0,max:30 }),
+        apartment: randNumber({ min: 0,max:120 }),
+        city: randCity(),
+        country: randCountry(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+
+      const direccionId = await queryInterface.bulkInsert('addresses', [direccion]);
+
+      usuario = {
+        name: randFirstName()+' '+randLastName(),
+        cuit: randNumber({ min: 11,max:99 })+'-'+randNumber({ min: 11111111,max:99999999 })+'-'+randNumber({ min: 11,max:99 }),
+        email: randEmail(),
+        password: randPassword(),
+        phone: randPhoneNumber(),
+        addressId: direccionId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+
+      const userId = await queryInterface.bulkInsert('users', [usuario]);
+  
+      nosocomio = {
+        bussinesName: randCompanyName(),
+        userId:userId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+
+      queryInterface.bulkInsert('nosocomios', [nosocomio]);
+    }
+
+    return true;
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
