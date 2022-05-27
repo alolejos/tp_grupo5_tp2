8191ae22921e92297ad84617da916ebbb5d863aa
Marcelo <marcelogomezgrois@gmail.com>
2022-05-26 22:02:48 -0300

    agregada la relación

--- src/db/models/administrator.js
+++ src/db/models/administrator.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Administrador.belongsTo(models.User);
     }
   }
   Administrator.init(
