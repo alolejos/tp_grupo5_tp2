0894da189fdd8326b792767883bd9f3371184ea4
Adrian <adrianclaret@gmail.com>
2022-06-04 19:00:33 -0300

    Agrego alta de paciente

--- src/routes/pacientes.js
+++ src/routes/pacientes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.get("/getAll", pacientes.getPacientes);
 router.get("/getById/:id", pacientes.getPacienteById);
 
+router.post("/add", pacientes.add);
 router.patch("/updateProfile",pacientes.updateProfile);
 
 module.exports = router
