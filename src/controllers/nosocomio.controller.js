b92247e42b8920465e711a3d2729dd721cb2c1d2
GDiamend <Gdiamend@gmail.com>
2022-06-09 20:36:12 -0300

    agrego el método add funcionando

--- src/controllers/nosocomio.controller.js
+++ src/controllers/nosocomio.controller.js
@@ -28,3 +28,75 @@ exports.getNosocomioById = async (req, res) => {
     }
 
 }
+
+exports.add = async (req, res) => {
+    let datosNosocomio = req.body;
+
+    let bussinesName = datosNosocomio.bussinesName;
+    let name = datosNosocomio.name;
+    let cuit = datosNosocomio.cuit;
+    let email = datosNosocomio.email;
+    let password = datosNosocomio.password;
+    let phone = datosNosocomio.phone;
+
+
+    //debería agregar validaciones
+
+    let usuario = {
+        name: name,
+        cuit: cuit,
+        email: email,
+        password: password,
+        phone: phone,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    };
+
+    try {
+        let userId;
+        models.User.create(usuario).then(function (resultado) {
+            userId = resultado.id;
+
+            models.Nosocomio.create({
+
+                userId: userId,
+                bussinesName: bussinesName,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+            ).then(function (resultado) {
+                res.status(200).send('Nosocomio creado');
+            });
+        });
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+
+
+}
+
+exports.deleteNosocomio = async (req, res) => {
+    let idNosocomio = req.body.idNosocomio;
+    try {
+        console.log("test")
+        const nosocomio = await models.Nosocomio.findOne({
+            where: {
+                id: idNosocomio
+            },
+            include: ['User']
+        });
+        console.log("2d test")
+        console.log(nosocomio);
+
+        if (nosocomio) {
+            models.Nosocomio.destroy({ where: { id: idNosocomio } });
+            res.status(200).send('Nosocomio eliminado');
+        } else {
+            res.status(500).send('No existe');
+        }
+    }
+    catch (error) {
+        res.status(500).send('El nosocomio no existe');
+    }
+}
--- src/routes/nosocomios.js
+++ src/routes/nosocomios.js
@@ -1,8 +1,10 @@
 const nosocomios = require('../controllers/nosocomio.controller');
 const express = require('express');
 const router = express.Router();
-module.exports = router
 
 router.get("/getAll", nosocomios.getNosocomios);
 router.get("/getById/:id", nosocomios.getNosocomioById);
+router.post("/add", nosocomios.add);
+router.delete("/deleteById/:id", nosocomios.deleteNosocomio);
 
+module.exports = router
